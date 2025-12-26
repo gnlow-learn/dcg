ec5169211ad1e71bc190aeaa6eaeda1996d6d578
Gnlow <gnlow@pm.me>
2025-12-27 00:20:32 +0900

    josa

--- sketch/a.pl
+++ sketch/a.pl
@@ -1,39 +1,58 @@
 :- use_module(library(dcgs)).
 
-sentence(ko, s(Type, Voice, Aspect, pred(V, agent(Subj, SD), obj(Obj, OD)))) -->
-    sentence_ko_sov(s(Type, Voice, Aspect, pred(V, agent(Subj, SD), obj(Obj, OD)))).
-sentence(ko, s(Type, Voice, Aspect, pred(V, agent(Subj, SD), obj(Obj, OD)))) -->
-    sentence_ko_osv(s(Type, Voice, Aspect, pred(V, agent(Subj, SD), obj(Obj, OD)))).
+char_upper(L, U) :-
+    once((char_code(L, C), C >= 97, C =< 122, U_code is C - 32, char_code(U, U_code); U = L)).
 
-sentence_ko_sov(s(decl, active, past, pred(V, agent(Subj, SD), obj(Obj, OD)))) -->
-    article(ko, SD), noun(ko, Subj), " 가 ", 
-    article(ko, OD), noun(ko, Obj), " 을 ", 
+% ===
+
+sentence(ko, s(D, V, A, Prop)) --> sentence_ko_sov(s(D, V, A, Prop)).
+sentence(ko, s(D, V, A, Prop)) --> sentence_ko_osv(s(D, V, A, Prop)).
+
+sentence_ko_sov(s(decl, active, past, pred(V, agent(S, SD), obj(O, OD)))) -->
+    noun_phrase(ko, S, SD, josa_subj), " ",
+    noun_phrase(ko, O, OD, josa_obj), " ",
     verb(ko, V, past).
 
-sentence_ko_osv(s(decl, active, past, pred(V, agent(Subj, SD), obj(Obj, OD)))) -->
-    article(ko, OD), noun(ko, Obj), " 을 ", 
-    article(ko, SD), noun(ko, Subj), " 가 ", 
+sentence_ko_osv(s(decl, active, past, pred(V, agent(S, SD), obj(O, OD)))) -->
+    noun_phrase(ko, O, OD, josa_obj), " ",
+    noun_phrase(ko, S, SD, josa_subj), " ",
     verb(ko, V, past).
 
-sentence(en, s(decl, active, past, pred(V, agent(Subj, det(SubjDet)), obj(Obj, det(ObjDet))))) -->
-    article(en, SubjDet), " ", noun(en, Subj), " ",
-    verb(en, V, past), " ",
-    article(en, ObjDet), " ", noun(en, Obj).
+noun_phrase(ko, N, _, JosaType) -->
+    noun(ko, N),
+    josa(N, JosaType).
 
 noun(ko, butterfly) --> "나비".
 noun(ko, flower)    --> "꽃".
 
+verb(ko, see, past) --> "보았다".
+
+josa(butterfly, josa_subj) --> "가".
+josa(flower, josa_subj)    --> "이".
+josa(butterfly, josa_obj)  --> "를".
+josa(flower, josa_obj)     --> "을".
+
+% ===
+
+sentence(en, IR) -->
+    { var(IR) }, !,
+    en_core(IR).
+sentence(en, IR) -->
+    { nonvar(IR), phrase(en_core(IR), [First|Rest]) },
+    { char_upper(First, Upper) },
+    [Upper], Rest.
+
+en_core(s(decl, active, T, pred(V, agent(S, SD), obj(O, OD)))) -->
+    { var(SD) -> SD = a ; true },
+    { var(OD) -> OD = a ; true },
+    article(en, SD), " ", noun(en, S), " ",
+    verb(en, V, T), " ",
+    article(en, OD), " ", noun(en, O).
+
 noun(en, butterfly) --> "butterfly".
 noun(en, flower)    --> "flower".
 
-verb(ko, see, past) --> "보았다".
-verb(ko, see, pres) --> "본다".
-
 verb(en, see, past) --> "saw".
-verb(en, see, pres) --> "sees".
-
-article(ko, _) --> "".
 
 article(en, a) --> "a".
 article(en, the) --> "the".
-article(en, none) --> "a".
--- test.ts
+++ test.ts
@@ -3,15 +3,14 @@ import { run } from "./src/run.ts"
 await Array.fromAsync(run
     (await Deno.readTextFile("sketch/a.pl"))
     (`
-        phrase(sentence(_, IR), "나비 가 꽃 을 보았다"),
+        phrase(sentence(_, IR), "나비가 꽃을 보았다"),
         phrase(sentence(_, IR), Res).
     `)
 ).then(x => {
     console.log(
         x.map(answer =>
             answer.status == "success"
-                ? answer.answer.Res
-                : "<failure>"
+                ? answer.answer.Res: "<failure>"
         )
     )
 })
