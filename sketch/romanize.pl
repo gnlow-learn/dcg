9281b9df3e0dcb84cfc40b745e891764bf525ffe
Gnlow <gnlow@pm.me>
2025-12-27 03:27:49 +0900

    romanize 양방향

--- sketch/romanize.pl
+++ sketch/romanize.pl
@@ -1,6 +1,6 @@
 :- module(romanize, [romanize/2]).
-
 :- use_module(library(dcgs)).
+:- use_module(library(lists)).
 
 vowel_map(0, "a").  % ㅏ
 vowel_map(5, "e").  % ㅔ
@@ -24,39 +24,99 @@ cho_map(16, "t").  % ㅌ
 cho_map(17, "p").  % ㅍ
 cho_map(18, "h").  % ㅎ
 
-jong_map(1, "g").
-jong_map(4, "n").
-jong_map(7, "d").
-jong_map(8, "l").
-jong_map(16, "m").
-jong_map(17, "b").
-jong_map(19, "s").
-jong_map(21, "ng").
-jong_map(22, "j").
+jong_map(1, "g").   % ㄱ
+jong_map(4, "n").   % ㄴ
+jong_map(7, "d").   % ㄷ
+jong_map(8, "l").   % ㄹ
+jong_map(16, "m").  % ㅁ
+jong_map(17, "b").  % ㅂ
+jong_map(19, "s").  % ㅅ
+jong_map(21, "ng"). % ㅇ
+jong_map(22, "j").  % ㅈ
 
-translate([]) --> [].
-translate([C|Cs]) -->
-    { char_code(C, Code),
-      Code >= 44032, Code =< 55203 },
+romanize(Input, Output) :-
+    (   nonvar(Input)
+    ->  phrase(translate_to_eng(Input), Result),
+        (   is_list_of_integers(Result) -> atom_codes(Output, Result)
+        ;   atom_chars(Output, Result)
+        )
+    ;   nonvar(Output)
+    ->  (   atom(Output) -> atom_codes(Output, RomanList) 
+        ;   RomanList = Output 
+        ),
+        phrase(translate_to_kor(HangulCodes), RomanList),
+        atom_codes(Input, HangulCodes)
+    ).
+
+is_list_of_integers([]).
+is_list_of_integers([H|T]) :- integer(H), is_list_of_integers(T).
+
+translate_to_eng([]) --> [].
+translate_to_eng([C|Cs]) -->
+    { char_code(C, Code), Code >= 44032, Code =< 55203 },
     !,
-    { 
-        Base is Code - 44032,
-        ChoIdx is Base // 588,
-        JungIdx is (Base mod 588) // 28,
-        JongIdx is Base mod 28
-    },
+    { Base is Code - 44032,
+      ChoIdx is Base // 588,
+      JungIdx is (Base mod 588) // 28,
+      JongIdx is Base mod 28 },
     emit(ChoIdx, cho_map),
     emit(JungIdx, vowel_map),
     emit(JongIdx, jong_map),
-    translate(Cs).
-translate([C|Cs]) --> [C], translate(Cs).
+    translate_to_eng(Cs).
+translate_to_eng([C|Cs]) --> [C], translate_to_eng(Cs).
 
-emit(Idx, Map) --> 
-    { Goal =.. [Map, Idx, Lat], call(Goal) }, 
-    !, 
-    Lat.
+emit(Idx, Map) --> { Goal =.. [Map, Idx, Lat], call(Goal) }, !, Lat.
 emit(_, _) --> "".
 
-romanize(Input, Output) :-
-    phrase(translate(Input), Result),
-    atom_chars(Output, Result).
+translate_to_kor([]) --> [].
+translate_to_kor([Code|Cs]) -->
+    parse_syllable(Code),
+    !,
+    translate_to_kor(Cs).
+translate_to_kor([C|Cs]) --> [C], translate_to_kor(Cs).
+
+parse_syllable(Code) -->
+    get_cho(ChoIdx),
+    get_jung(JungIdx),
+    % 여기서 "으" (초성 11 + 중성 18) 인 경우를 차단
+    { \+ (ChoIdx = 11, JungIdx = 18) },
+    get_jong(JongIdx),
+    { Code is (ChoIdx * 588) + (JungIdx * 28) + JongIdx + 44032 }.
+
+get_cho(Idx) --> match_map(cho_map, Idx), !.
+get_cho(11)  --> [].
+
+get_jung(Idx) --> match_map(vowel_map, Idx), !.
+get_jung(18)  --> [].
+
+get_jong(Idx) -->
+    match_map(jong_map, Idx),
+    (   next_is_vowel -> { fail } ; { true } ),
+    !.
+get_jong(0) --> [].
+
+next_is_vowel(S, S) :-
+    S = [H|_],
+    vowel_map(_, [Vh|_]),
+    char_code_equal(H, Vh).
+
+char_code_equal(X, X) :- !.
+char_code_equal(C, Code) :- integer(Code), char_code(C, Code), !.
+char_code_equal(Code, C) :- integer(Code), char_code(C, Code).
+
+match_map(MapName, Idx) -->
+    { findall(Len-(Str-I), (
+        Goal =.. [MapName, I, Str], 
+        call(Goal), 
+        Str \= "", 
+        length(Str, Len)
+      ), Matches),
+      keysort(Matches, Sorted),
+      reverse(Sorted, Descending) },
+    select_match(Descending, Idx).
+
+select_match([_-(Str-Idx)|_], Idx) --> consume_str(Str).
+select_match([_|Rest], Idx) --> select_match(Rest, Idx).
+
+consume_str([]) --> [].
+consume_str([H|T]) --> [C], { char_code_equal(C, H) }, consume_str(T).
--- test.ts
+++ test.ts
@@ -5,13 +5,17 @@ use("romanize.pl")
 await Array.fromAsync(run
     (await Deno.readTextFile("sketch/main.pl"))
     (`
-        romanize("멜트", Res).
+        romanize(Res, "imelda"),
+        romanize("바람", Res2).
+        
     `)
 ).then(x => {
     console.log(
-        x.map(answer =>
-            answer.status == "success"
-                ? answer.answer.Res: "<failure>"
-        )
+        JSON.stringify(x, void 0, 2)
+        // x.map(answer =>
+        //     answer.status == "success"
+        //         ? answer.answer.Res
+        //         : "<failure>"
+        // )
     )
 })
