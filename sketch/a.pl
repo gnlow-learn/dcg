4f3f321b8ed50b3e8b2b51be06195fe9d969cda5
Gnlow <gnlow@pm.me>
2025-12-27 00:07:43 +0900

    꽃 을 나비 가 보았다

--- sketch/a.pl
+++ sketch/a.pl
@@ -1,8 +1,18 @@
 :- use_module(library(dcgs)).
 
-sentence(ko, s(decl, active, past, pred(V, agent(Subj, det(SubjDet)), obj(Obj, det(ObjDet))))) -->
-    article(ko, SubjDet), noun(ko, Subj), " 가 ", 
-    article(ko, ObjDet), noun(ko, Obj), " 을 ", 
+sentence(ko, s(Type, Voice, Aspect, pred(V, agent(Subj, SD), obj(Obj, OD)))) -->
+    sentence_ko_sov(s(Type, Voice, Aspect, pred(V, agent(Subj, SD), obj(Obj, OD)))).
+sentence(ko, s(Type, Voice, Aspect, pred(V, agent(Subj, SD), obj(Obj, OD)))) -->
+    sentence_ko_osv(s(Type, Voice, Aspect, pred(V, agent(Subj, SD), obj(Obj, OD)))).
+
+sentence_ko_sov(s(decl, active, past, pred(V, agent(Subj, SD), obj(Obj, OD)))) -->
+    article(ko, SD), noun(ko, Subj), " 가 ", 
+    article(ko, OD), noun(ko, Obj), " 을 ", 
+    verb(ko, V, past).
+
+sentence_ko_osv(s(decl, active, past, pred(V, agent(Subj, SD), obj(Obj, OD)))) -->
+    article(ko, OD), noun(ko, Obj), " 을 ", 
+    article(ko, SD), noun(ko, Subj), " 가 ", 
     verb(ko, V, past).
 
 sentence(en, s(decl, active, past, pred(V, agent(Subj, det(SubjDet)), obj(Obj, det(ObjDet))))) -->
--- test.ts
+++ test.ts
@@ -4,6 +4,14 @@ await Array.fromAsync(run
     (await Deno.readTextFile("sketch/a.pl"))
     (`
         phrase(sentence(_, IR), "나비 가 꽃 을 보았다"),
-        phrase(sentence(_, IR), En).
+        phrase(sentence(_, IR), Res).
     `)
-).then(x => console.log(x))
+).then(x => {
+    console.log(
+        x.map(answer =>
+            answer.status == "success"
+                ? answer.answer.Res
+                : "<failure>"
+        )
+    )
+})
