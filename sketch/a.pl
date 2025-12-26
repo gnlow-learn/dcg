21008451fa4cc4fce107fb3c0eddf96802a191e1
Gnlow <gnlow@pm.me>
2025-12-26 23:41:03 +0900

    Create a.pl

---
+++ sketch/a.pl
@@ -0,0 +1,37 @@
+:- use_module(library(dcgs)).
+:- use_module(library(lists)).
+
+ko_to_en(KoStr, EnStr) :-
+    phrase(ko_sentence(IR), KoStr),
+    phrase(en_sentence(IR), EnStr).
+
+en_to_ko(EnStr, KoStr) :-
+    phrase(en_sentence(IR), EnStr),
+    phrase(ko_sentence(IR), KoStr).
+
+ko_sentence(s(decl, active, past, pred(V, agent(Subj, det(none)), obj(Obj, det(none))))) -->
+    noun_ko(Subj), " 가 ", 
+    noun_ko(Obj), " 을 ", 
+    verb_ko(V, past).
+
+noun_ko(butterfly) --> "나비".
+noun_ko(flower)    --> "꽃".
+
+verb_ko(see, past) --> "보았다".
+verb_ko(see, pres) --> "본다".
+
+en_sentence(s(decl, active, past, pred(V, agent(Subj, det(SubjDet)), obj(Obj, det(ObjDet))))) -->
+    article(SubjDet), " ", noun_en(Subj), " ",
+    verb_en(V, past), " ",
+    article(ObjDet), " ", noun_en(Obj).
+
+article(a) --> "a".
+article(a) --> "A".
+article(the) --> "the".
+article(the) --> "The".
+
+noun_en(butterfly) --> "butterfly".
+noun_en(flower)    --> "flower".
+
+verb_en(see, past) --> "saw".
+verb_en(see, pres) --> "sees".
